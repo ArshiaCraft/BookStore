329e9e91f4eacbd56cc160c005f7cd105305296f
ArshiaCraft <arshiaabdigg@gmail.com>
2023-11-19 20:26:16 +0330

    first commit

---
+++ src/Acme.BookStore.Blazor/BookStoreBlazorAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using Acme.BookStore.Books;
+using Acme.BookStore.Authors;
+using AutoMapper;
+
+namespace Acme.BookStore.Blazor;
+
+public class BookStoreBlazorAutoMapperProfile : Profile
+{
+    public BookStoreBlazorAutoMapperProfile()
+    {
+        CreateMap<BookDto, CreateUpdateBookDto>();
+        CreateMap<AuthorDto, UpdateAuthorDto>();
+
+    }
+}
